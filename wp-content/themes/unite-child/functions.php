e26425396a1e83751717e04048e2a5271e5fda6c
Rahil Patel <rhitsol.dev@gmail.com>
2018-07-08 18:51:46 +0530

    created unite-child theme and added necessary files

---
+++ wp-content/themes/unite-child/functions.php
@@ -0,0 +1,18 @@
+<?php
+if ( ! function_exists( 'unite_child_scripts' ) ) :
+    /**
+     * Enqueue Styles.
+     */
+    function unite_child_scripts() {
+
+        $parent_style = 'unite-parent-style';
+
+        wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+        wp_enqueue_style( 'unite-child-style',
+            get_stylesheet_directory_uri() . '/style.css',
+            array( $parent_style ),
+            wp_get_theme()->get('Version')
+        );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'unite_child_scripts' );
